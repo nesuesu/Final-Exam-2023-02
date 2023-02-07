bc79bde90260067fc44af4054fd6dd04e348b333
nesuesu <viktor.lavrinovic@gmail.com>
2023-02-08 00:26:26 +0200

    date and time added to theeach answer

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -5,9 +5,13 @@ const Answer = ({answer}) => {
 
     const { questions } = useContext(QuestionContext);
 
+    console.log('answer -',answer);
+    console.log('questions -',questions);
+
     return (
         <>
-        <p>question : {questions.find(question => question.id === answer.questionId).question}</p>
+        <p>question : {questions.find(question => question.id.toString() === answer.questionId.toString()).question}</p>
+        <p>Date : { new Date(answer.id).toLocaleDateString('LT')  } {new Date(answer.id).toLocaleTimeString('LT')}</p>
         <p>answer id: {answer.id} question id: {answer.questionId} user id: {answer.userId}</p>
         <p>{answer.answer}</p>
         </>
