ecdfd0c1302e9a696520f302e82c31a0c8f5e32e
nesuesu <viktor.lavrinovic@gmail.com>
2023-02-08 09:30:02 +0200

    basic design

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,6 @@
 
+import logo from './images/logo.png'
+
 import { Link } from "react-router-dom";
 
 import { useContext } from "react";
@@ -10,12 +12,26 @@ const Header = () => {
 
     return (
         <>
-        <Link to={'/login'}>Login</Link>
-        <Link to={'/register'}>Signup</Link>
-        <Link to={'/questions'}>Questions</Link>
-        <Link to={'/answers'}>Answers</Link>
-        <Link to={'/addquestion'}>Add question</Link>
-        <Link to={'/addanswer'}>Add Answer</Link>
+        <header>
+
+        <div className='logo'>
+            <img src={logo} alt="logo" />
+        </div>
+
+        <div className='menu'>
+            <Link to={'/questions'}>Questions</Link>
+            <Link to={'/addquestion'}>Add question</Link>
+            <Link to={'/answers'}>Answers</Link>
+            <Link to={'/addanswer'}>Add Answer</Link>
+        </div>
+        
+        <div className='login'>
+            <Link to={'/login'}>Login</Link>
+            <Link to={'/register'}>Signup</Link>
+        </div>
+
+        </header>
+
         <h1>Logged in: {loggedInUser && loggedInUser.username}</h1>
         </>
     );
